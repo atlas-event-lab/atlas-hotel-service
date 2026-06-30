2d8a4bf58b9e7676e14bb1aad56b06d17f5ffdb7
Eduardo Pillco <eduardo_pillco@unicomer.com>
2026-06-30 15:06:31 -0500

    Update actuator ports and cors

--- src/main/java/com/atlas/hotel/config/SecurityConfig.java
+++ src/main/java/com/atlas/hotel/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,6 +39,7 @@ public class SecurityConfig {
     };
 
     return http
+        .cors(Customizer.withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
